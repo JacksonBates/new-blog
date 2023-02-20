9a6b784343fba47a5f174f86cfb7ec0a840bfa0b
jacksonbates <jacksonbates@hotmail.com>
2023-02-20 19:28:37 +1100

    amend read token for new branch name

--- .tina/config.js
+++ .tina/config.js
@@ -6,7 +6,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 export default defineConfig({
 	branch,
 	clientId: "9e36169c-930b-4705-aa1f-0f9a874ca155", // Get this from tina.io
-	token: "a073a1923e303a0f21b7cda29341bdec51b98463", // Get this from tina.io
+	token: "f3ef0ea02d55ff5438ae7c1b698e257fa79d516e", // Get this from tina.io
 	build: {
 		outputFolder: "admin",
 		publicFolder: "public",
